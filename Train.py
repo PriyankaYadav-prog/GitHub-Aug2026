2140a190d5eff74f209e8dcc0bfd8fc2b932a467
PriyankaYadav-prog <panku9708@gmail.com>
2026-09-04 16:42:34 +0530

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,6 +1,4 @@
-from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
 model = LogisticRegression()
-model.fit(load_iris().data, load_iris().target)
-print("Model trained with accuracy:", model.score(load_iris().data, load_iris().target))
+model.fit(X_train, y_train)
